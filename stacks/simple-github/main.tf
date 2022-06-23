4620e6f3d2e53f265410adf5f33020304feb00b9
wojciech12 <wojciechb@spacelift.io>
2022-06-23 15:58:18 +0200

    tf

--- stacks/simple-github/main.tf
+++ stacks/simple-github/main.tf
@@ -9,7 +9,7 @@ provider "github" {
 
 resource "github_repository" "my_repo" {
   name        = "tf_sp_example"
-  description = "My even more more awesome codebase"
+  description = "My even more more more awesome codebase"
 
   visibility = "public"
 }
