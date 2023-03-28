68d0b62c9e00c7bd7d24eed18cc1f9f1d3375dfd
wojciech12 <wojciechb@spacelift.io>
2023-03-28 12:45:50 +0200

    update dsc

--- stacks/simple-github/main.tf
+++ stacks/simple-github/main.tf
@@ -4,7 +4,7 @@ provider "github" {
 
 resource "github_repository" "my_repo" {
   name        = "tf_sp_example"
-  description = "My amazing repository"
+  description = "My amazing repository ABC"
 
   visibility = "public"
 }
