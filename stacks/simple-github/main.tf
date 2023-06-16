dd1ac4804cdfafd42f5ca8554bce07611638a8f0
Przemyslaw <przemyslaww@spacelift.io>
2023-06-16 15:34:16 +0200

    test for each

--- stacks/simple-github/main.tf
+++ stacks/simple-github/main.tf
@@ -5,9 +5,24 @@ provider "github" {
   owner = var.github_owner
 }
 
+variable "repos" {
+  description = "repos_to_be_created"
+  type = list(object({
+    name                = string
+    description         = string
+  }))
+}
+
 resource "github_repository" "my_repo" {
   name        = "tf_sp_example"
   description = "My new repository for Terraform"
 
+  visibility = "public"
+}
+
+resource "github_repository" "repos" {
+  for_each = toset(var.repos)
+  name        = each.value.name
+  description = each.value.description
   visibility = "public"
 }
